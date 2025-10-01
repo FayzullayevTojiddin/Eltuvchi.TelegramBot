f969ad853d37e86189c10290984de3999c78f0ea
FayzullayevTojiddin <tfayzullaev66@gmail.com>
2025-10-01 20:04:09 +0500

    Has Problem

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     API_TOKEN="8326509962:AAFtx8cRjeu94eKDerlrNQSgtF-hDzg0CCI"
     PORT=1000
-    WEBHOOK_URL="https://mtaxi.uz/bot"
-    WEBHOOK_PATH = "/webhook"
+    WEBHOOK_URL="https://mtaxi.uz"
+    WEBHOOK_PATH = "/bot/webhook"
